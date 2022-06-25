a35179b175339ee25e2d47c1092d1fd4e2d840ac
tracyspacy <spacyearn@tutanota.com>
2022-06-25 16:27:31 +0200

    Update lemmy_register.sol

--- contracts/lemmy_register.sol
+++ contracts/lemmy_register.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 /// @title A simple register contract
 contract Lemmy_register {
     uint256 public max = 1000;
     uint256 public registred_count;
     mapping(address => bool) public isRegistred;
 
-    /// @dev Puts caller address into the register
+    /// @dev Adds caller address into the register
     function register_address() public {
         require(
             bool(isRegistred[msg.sender]) == false,
