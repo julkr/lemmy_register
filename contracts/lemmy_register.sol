a2a069255005f18a63ac02e901ead26e953c739e
tracyspacy <spacyearn@tutanota.com>
2022-06-25 16:25:42 +0200

    prettified

--- contracts/lemmy_register.sol
+++ contracts/lemmy_register.sol
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+/// @title A simple register contract
+contract Lemmy_register {
+    uint256 public max = 1000;
+    uint256 public registred_count;
+    mapping(address => bool) public isRegistred;
 
-contract Lemmy_register{
-  uint256 public max = 1000;
-  uint256 public registred_count;
-  mapping(address => bool) public isRegistred;
-
-  function register_address() public {
-    require(bool(isRegistred[msg.sender])==false, "Address is already registred");
-    require(uint256(registred_count)<uint256(max), "Register max is already reached");
-    registred_count += 1;
-    isRegistred[msg.sender] = true;
-  }
-
+    /// @dev Puts caller address into the register
+    function register_address() public {
+        require(
+            bool(isRegistred[msg.sender]) == false,
+            "Address is already registred"
+        );
+        require(
+            uint256(registred_count) < uint256(max),
+            "Register max is already reached"
+        );
+        registred_count += 1;
+        isRegistred[msg.sender] = true;
+    }
 }
